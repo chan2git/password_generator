40561a57d2731400ddf317a6b61b6146e8b77fcc
chan2git <138939203+chan2git@users.noreply.github.com>
2023-07-17 20:08:08 -0400

    Update password_generator.py
    - Added user input validation
    - Added option for user to end script
    - Updated special characters to custom list of commonly accepted characters

--- password_generator.py
+++ password_generator.py
@@ -1,30 +1,36 @@
-# Import the random and string modules
+#############################
+# Version:    1.01          #
+# Date:       07/17/2023    #
+# Coder:      CH @chat2git  #
+#############################
+
+### Import the random, string, sys modules
 import random
 import string
+import sys
 
-# Create a function that will accept an argument referencing the length of the password
+### Create a function that will accept an argument referencing the length of the password
 def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for i in range(length))                # List expression is used here. For i in range of length, randomly choose an item from characters and join it to password, which starts off as ''
+    accepted_specialchar = "!@#$%^&*_-+=<>?~"                                                 # Assign a custom string of commonly accepted special characters for passwords
+    characters = string.ascii_letters + string.digits + accepted_specialchar                        
+    password = ''.join(random.choice(characters) for i in range(length))                            # List expression is used here. For i in range of length, randomly choose an item from characters and join it to password, which starts off as ''
     return password
 
-# Prompt the user for the length of their password
-length = int(input("Please enter the length of the generated password: "))
 
 
-# Call the generate_password function to generate and return the password to the user
-password = generate_password(length)
-print("Your generated password is: ", password)
+### While condition is true, continue to prompt user for action
+while True:
+    user_length = input("Please enter the length of the generated password, or Q to quit: ")        # Prompts the user to enter the length of desired password
+
+    if user_length.isdigit() and int(user_length) > 0:                                              # Checks if the user input is a positive integer
+        user_length = int(user_length)                                                              # If conditions pass, officially convert user input to a int value and break the while loop
+        break      
+    elif user_length.upper() == "Q":                                                                # If user input is Q, exit this python script
+        print(". . . Quitting . . .")
+        sys.exit()                                                              
+    else:
+        print("Invalid input. Please enter a positive interger only.")
 
 
-
-
-
-
-
-
-
-# Use this to see what all the characters are being assigned to from the string modules to the variable
-#
-#test_Characters = string.ascii_letters + string.digits + string.punctuation
-#print(test_Characters)
+password = generate_password(user_length)                                                           # Call the generate_password function to generate and return the password to the user
+print(f"Your generated password is: {password}")
